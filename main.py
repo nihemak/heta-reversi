60ae4d8467b703b5c5b96c02bdde5e6e93c29089
Makoto Nihei <nihemak@gmail.com>
2018-11-01 05:09:43 +0900

    display black and white

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # coding: utf-8
 
+import numpy as np
+
 def render_board(board_data):
+    black = "○"  # 1
+    white = "●"  # -1
+    board = [i if v == 0 else " {} ".format(black if v == 1 else white) for i, v in enumerate(board_data)]
     row = " {:>3} | {:>3} | {:>3} | {:>3} | {:>3} | {:>3} | {:>3} | {:>3} "
     hr = "\n------------------------------------------------\n"
     layout = row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row + hr + row
-    print((layout).format(*board_data))
+    print((layout).format(*board))
 
 def main():
-    board = range(64)
+    board = np.array([0] * 64, dtype=np.float32)
+    board[28] = board[35] = 1
+    board[27] = board[36] = -1
     render_board(board)
 
 if __name__ == "__main__":
