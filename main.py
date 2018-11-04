6efc3773ae1e2b1706ac7d96ae5c47194df4a2f0
Makoto Nihei <nihemak@gmail.com>
2018-11-04 11:23:35 +0900

    play random vs random

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 import numpy as np
 
+def get_init_board():
+    board = np.array([0] * 64, dtype=np.float32)
+    board[28] = board[35] = 1
+    board[27] = board[36] = -1
+    return board
+
 def get_flip_positions_by_row_column(board, is_black, row, column, row_add, column_add):
     position_nums = []
 
@@ -60,6 +66,9 @@ def is_putable_position_num(board, is_black, position_num):
 def get_putable_position_nums(board, is_black):
     return [num for num in range(64) if is_putable_position_num(board, is_black, num)]
 
+def is_putable(board, is_black):
+    return len(get_putable_position_nums(board, is_black)) > 0
+
 def render_board(board, is_black):
     black, white = "○", "●"  # 1, -1
     display_board = [i if v == 0 else " {} ".format(black if v == 1 else white) for i, v in enumerate(board)]
@@ -69,24 +78,22 @@ def render_board(board, is_black):
     print((layout).format(*display_board))
     print("{}: putable position numbers are {}".format(black if is_black else white, get_putable_position_nums(board, is_black)))
 
+def is_end(board):
+    return not (is_putable(board, True) or is_putable(board, False))
+
 def main():
-    board = np.array([0] * 64, dtype=np.float32)
-    board[28] = board[35] = 1
-    board[27] = board[36] = -1
+    board = get_init_board()
 
     is_black = True
-    render_board(board, is_black)
-    position_num = np.random.choice(get_putable_position_nums(board, is_black))
-    print(position_num)
-    board = put(board, is_black, position_num)
-
-    is_black = False
-    render_board(board, is_black)
-    position_num = np.random.choice(get_putable_position_nums(board, is_black))
-    print(position_num)
-    board = put(board, is_black, position_num)
-
-    is_black = True
+    while not is_end(board):
+        render_board(board, is_black)
+        if is_putable(board, is_black):
+            position_num = np.random.choice(get_putable_position_nums(board, is_black))
+            print(position_num)
+            board = put(board, is_black, position_num)
+        else:
+            print("pass")
+        is_black = not is_black
     render_board(board, is_black)
 
 if __name__ == "__main__":
