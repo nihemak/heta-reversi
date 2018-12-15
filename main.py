210c78d0b78caa68b4484499e1e9a9d58745a9ab
Makoto Nihei <nihemak@gmail.com>
2018-12-15 23:08:27 +0900

    add sample chainer model

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import csv
 import datetime
 import sys
 import chainer
+import chainer.functions as F
+import chainer.links as L
 
 def get_init_board():
     board = np.array([0] * 64, dtype=np.float32)
@@ -236,6 +238,33 @@ class ChoiceMonteCarloTreeSearch:
         index = self._choice_node_index(nodes)
         choice = nodes[index]['position_num']
         return choice
+
+class SampleChain(chainer.Chain):
+    def __init__(self, class_labels=10):
+        super(SampleChain, self).__init__()
+        with self.init_scope():
+            self.conv1_1 = L.Convolution2D(None, 16, ksize=5, pad=2, nobias=True)
+            self.conv1_2 = L.Convolution2D(None, 16, ksize=5, pad=2, nobias=True)
+            self.conv2_1 = L.Convolution2D(None, 32, ksize=3, pad=1, nobias=True)
+            self.conv2_2 = L.Convolution2D(None, 32, ksize=3, pad=1, nobias=True)
+            self.fc1 = L.Linear(None, 512, nobias=True)
+            self.fc2 = L.Linear(None, class_labels, nobias=True)
+
+    def __call__(self, x):
+        conv1_1 = self.conv1_1(x)
+        conv1_1 = F.relu(conv1_1)
+        conv1_2 = self.conv1_2(conv1_1)
+        conv1_2 = F.relu(conv1_2)
+        pool1 = F.max_pooling_2d(conv1_2, ksize=2, stride=2)
+        conv2_1 = self.conv2_1(pool1)
+        conv2_1 = F.relu(conv2_1)
+        conv2_2 = self.conv2_2(conv2_1)
+        conv2_2 = F.relu(conv2_2)
+        pool2 = F.max_pooling_2d(conv2_2, ksize=2, stride=2)
+        fc1 = self.fc1(pool2)
+        fc1 = F.relu(fc1)
+        fc2 = self.fc2(fc1)
+        return fc2
 
 def choice_human(player):
     _, _, putable_position_nums = player
