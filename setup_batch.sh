e6ec22e2dd7abb2f052c8aeac98d648f9a9c7f97
Makoto Nihei <nihemak@gmail.com>
2019-01-21 07:34:36 +0900

    add internet gateway to setup_batch

--- setup_batch.sh
+++ setup_batch.sh
@@ -6,3 +6,14 @@ VPC_ID=$(echo ${VPC} | jq -r ".Vpc.VpcId")
 aws ec2 create-tags \
     --resources ${VPC_ID} \
     --tags Key=Name,Value=test-batch
+
+## Create Internet Gateway
+INTERNET_GATEWAY=$(aws ec2 create-internet-gateway)
+INTERNET_GATEWAY_ID=$( \
+    echo ${INTERNET_GATEWAY} | jq -r ".InternetGateway.InternetGatewayId")
+aws ec2 create-tags \
+    --resources ${INTERNET_GATEWAY_ID} \
+    --tags Key=Name,Value=test-batch
+aws ec2 attach-internet-gateway \
+    --internet-gateway-id ${INTERNET_GATEWAY_ID} \
+    --vpc-id ${VPC_ID}
