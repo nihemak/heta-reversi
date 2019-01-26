9353a998746c546e6506c867181f5b7643f2e63a
Makoto Nihei <nihemak@gmail.com>
2019-01-26 11:44:23 +0900

    add ecr

--- setup_batch.sh
+++ setup_batch.sh
@@ -61,3 +61,95 @@ DEFAULT_SECURITY_GROUP_ID=$( \
     aws ec2 describe-security-groups \
         --filters Name=group-name,Values=default Name=vpc-id,Values=${VPC_ID} \
             | jq -r '.SecurityGroups[].GroupId')
+
+## Create S3
+aws s3 mb s3://test-batch-bucket-name --region ap-northeast-1
+
+## Create ECR repository
+ECR_REPO_NAME="test-batch"
+ECR_REPO=$(aws ecr create-repository --repository-name ${ECR_REPO_NAME})
+ECR_REPO_URL=$(echo ${ECR_REPO} | jq -r ".repository.repositoryUri")
+
+## Create IAM ecr build role
+cat <<EOF > Trust-Policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "codebuild.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+ROLE_ECR_BUILD=$(aws iam create-role --role-name test-batch-build-ecr \
+                                     --assume-role-policy-document file://Trust-Policy.json)
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess \
+                           --role-name test-batch-build-ecr
+ROLE_ECR_BUILD_ARN=$(echo ${ROLE_ECR_BUILD} | jq -r ".Role.Arn")
+
+## Create CodeBuild ecr
+cat <<EOF > Source.json
+{
+  "type": "CODECOMMIT",
+  "location": "https://git-codecommit.ap-northeast-1.amazonaws.com/v1/repos/heta-reversi",
+  "buildspec": "buildspec_ecr.yml"
+}
+EOF
+cat <<EOF > Artifacts.json
+{
+  "type": "NO_ARTIFACTS"
+}
+EOF
+cat <<EOF > Environment.json
+{
+  "type": "LINUX_CONTAINER",
+  "image": "aws/codebuild/docker:18.09.0",
+  "computeType": "BUILD_GENERAL1_SMALL",
+  "environmentVariables": [
+    {
+      "name": "REGION",
+      "value": "ap-northeast-1",
+      "type": "PLAINTEXT"
+    },
+    {
+      "name": "IMAGE_REPO_NAME",
+      "value": "example",
+      "type": "PLAINTEXT"
+    },
+    {
+      "name": "IMAGE_TAG",
+      "value": "latest",
+      "type": "PLAINTEXT"
+    },
+    {
+      "name": "ECR_REPO_URL",
+      "value": "${ECR_REPO_URL}",
+      "type": "PLAINTEXT"
+    }
+  ]
+}
+EOF
+aws codebuild create-project --name test-batch-ecr \
+                               --source file://Source.json \
+                               --artifacts file://Artifacts.json \
+                               --environment file://Environment.json \
+                               --service-role ${ROLE_ECR_BUILD_ARN}
+CODEBUILD_ID=$(aws codebuild start-build --project-name test-batch-ecr | tr -d "\n" | jq -r '.build.id')
+echo "started.. id is ${CODEBUILD_ID}"
+while true
+do
+  sleep 10s
+  STATUS=$(aws codebuild batch-get-builds --ids "${CODEBUILD_ID}" | tr -d "\n" | jq -r '.builds[].buildStatus')
+  echo "..status is ${STATUS}."
+  if [ "${STATUS}" != "IN_PROGRESS" ]; then
+    if [ "${STATUS}" != "SUCCEEDED" ]; then
+      echo "faild."
+    fi
+  echo "done."
+  break
+  fi
+done
