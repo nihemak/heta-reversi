6933982769951d730f8865de430d0adca7e87983
Makoto Nihei <nihemak@gmail.com>
2019-02-04 07:59:19 +0900

    add CodeBuild to create nvidia-docker2 ami of AWS batch

--- setup_batch.sh
+++ setup_batch.sh
@@ -154,6 +154,84 @@ do
   fi
 done
 
+## Create IAM ami build role
+cat <<EOF > Trust-Policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "codebuild.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+ROLE_AMI_BUILD=$(aws iam create-role --role-name test-batch-build-ami \
+                                     --assume-role-policy-document file://Trust-Policy.json)
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess \
+                           --role-name test-batch-build-ami
+ROLE_AMI_BUILD_ARN=$(echo ${ROLE_AMI_BUILD} | jq -r ".Role.Arn")
+
+## Create CodeBuild ami
+cat <<EOF > Source.json
+{
+  "type": "CODECOMMIT",
+  "location": "https://git-codecommit.ap-northeast-1.amazonaws.com/v1/repos/heta-reversi",
+  "buildspec": "buildspec_ami.yml"
+}
+EOF
+cat <<EOF > Artifacts.json
+{
+  "type": "NO_ARTIFACTS"
+}
+EOF
+cat <<EOF > Environment.json
+{
+  "type": "LINUX_CONTAINER",
+  "image": "aws/codebuild/docker:18.09.0",
+  "computeType": "BUILD_GENERAL1_SMALL",
+  "environmentVariables": [
+    {
+      "name": "AWS_REGION",
+      "value": "ap-northeast-1",
+      "type": "PLAINTEXT"
+    },
+    {
+      "name": "AWS_SUBNET_ID",
+      "value": "${SUBNET_ID}",
+      "type": "PLAINTEXT"
+    }
+  ]
+}
+EOF
+aws codebuild create-project --name test-batch-ami \
+                               --source file://Source.json \
+                               --artifacts file://Artifacts.json \
+                               --environment file://Environment.json \
+                               --service-role ${ROLE_AMI_BUILD_ARN}
+CODEBUILD_ID=$(aws codebuild start-build --project-name test-batch-ami --source-version master | tr -d "\n" | jq -r '.build.id')
+echo "started.. id is ${CODEBUILD_ID}"
+while true
+do
+  sleep 10s
+  STATUS=$(aws codebuild batch-get-builds --ids "${CODEBUILD_ID}" | tr -d "\n" | jq -r '.builds[].buildStatus')
+  echo "..status is ${STATUS}."
+  if [ "${STATUS}" != "IN_PROGRESS" ]; then
+    if [ "${STATUS}" != "SUCCEEDED" ]; then
+      echo "faild."
+    fi
+    echo "done."
+    break
+  fi
+done
+AMI_IDS=$(aws ec2 describe-images \
+    --owner self \
+    --filters "Name=tag:USE,Values=heta-reversi" | jq -r ".Images[].ImageId")
+AMI_ID="${AMI_IDS[0]}"
+
 ## Create Instance role
 cat <<EOF > Trust-Policy.json
 {
