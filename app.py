4d3772ca87028804818d1a5d28591911bedfe15e
Makoto Nihei <nihemak@gmail.com>
2019-03-07 07:53:00 +0900

    Add reversi layout

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from Reversi import Reversi
+
 app = Flask(__name__)
+app.jinja_env.globals.update(chr=chr)
+app.jinja_env.globals.update(int=int)
 
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+def index():
+    board, is_black, putable_position_nums = Reversi.get_player(Reversi.get_init_board())
+    return render_template('index.html', board=board, is_black=is_black, putable_position_nums=putable_position_nums)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
